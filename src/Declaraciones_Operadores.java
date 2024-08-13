94f67b08587ebbb1a2278b19d099af96e19956ff
teban192 <mmonestelalf@gmail.com>
2024-08-13 13:52:13 -0600

    First commit

---
+++ src/Declaraciones_Operadores.java
@@ -0,0 +1,16 @@
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int operador1,operador2,resultado;
+		operador1=8;
+		operador2=7;
+		resultado= operador1+operador2;
+		System.out.println(resultado);
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad;
+		edad = 35;
+		System.out.println(edad);
+
+	}
+
+}
